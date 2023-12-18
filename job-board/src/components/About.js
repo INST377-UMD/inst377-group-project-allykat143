889846fbca742df560eec2711fad8ded823dd6d4
maritrotz <maritrotz@gmail.com>
2023-12-17 21:30:55 -0500

    Updated the About page

--- job-board/src/components/About.js
+++ job-board/src/components/About.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import trotzImage from './KTrotz.JPG';
+import clipperImage from './AClipper.jpg'
+import umdImage from './UMD_IMG.png';
 
 const About = () => {
   return (
     <div>
       <h2 id='aboutUs'>About Us</h2>
-      <p>This is a simple job board application designed to display open jobs for minorities.</p>
+      <p id = "header-text">Our team is dedicated to providing minorities with the necessary resources and tools to accomplish their career goals .</p>
       <div class = "img-container">
-        <img src={trotzImage} alt="Binary" width="450" height="580" />
-      </div>
+        <div class = "img-item">
+          <img src={trotzImage} alt="KTrotz"/>
+          <h2><u>Kamari Trotz</u></h2>
+          <p id="about_text">A senior Information Science Major looking to pursue a career in Data Science/Analytics.</p>
+        </div>
+        <div class = "img-item">
+          <img src={clipperImage} alt="AClipper" />
+          <h2><u>Allyson Clipper</u></h2>
+          <p id="about_text">A senior Information Science Major looking to pursue a career in Software Development.</p>
+        </div>
+        <div class = "img-item">
+          <img src={umdImage} alt="MMedina"/>
+          <h2><u>Michelle Medina</u></h2>
+          <p id="about_text">A senior Information Science Major looking to pursue a career in Data Science.</p>
+        </div>
+        </div>
     </div>
   );
 };
